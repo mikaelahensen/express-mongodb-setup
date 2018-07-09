dac87bed7f99399d262f7e4b6260e14a6be15de7
Mikaela Hensen <mikaelahensen@Mikaelas-MacBook-Pro-2.local>
2018-07-09 12:55:09 -0500

    updates heroku port

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ var bodyParser = require('body-parser');
 const initializeDatabases = require('./dbs')
 const routes = require('./routes')
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+// var allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  next();
-}
+//   next();
+// }
 
-app.use(allowCrossDomain);
+// app.use(allowCrossDomain);
 
 initializeDatabases().then(dbs => {
   // Initialize the application once database connections are ready.
-  routes(app, dbs).listen(3000, () => console.log('Listening on port 3000'))
+  routes(app, dbs).listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'))
 }).catch(err => {
   console.error('Failed to make all database connections!')
   console.error(err)
