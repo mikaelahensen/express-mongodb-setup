36efd82cdc5184e25b9d4d0ceaebc4b6a0046a51
Mikaela Hensen <mikaelahensen@Mikaelas-MacBook-Pro-2.local>
2018-07-09 12:46:21 -0500

    update cors

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ var bodyParser = require('body-parser');
 const initializeDatabases = require('./dbs')
 const routes = require('./routes')
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
+
 initializeDatabases().then(dbs => {
   // Initialize the application once database connections are ready.
   routes(app, dbs).listen(3000, () => console.log('Listening on port 3000'))
+  app.use(allowCrossDomain);
 }).catch(err => {
   console.error('Failed to make all database connections!')
   console.error(err)
